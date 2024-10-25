bead429b908548275d01279b57f5546bc131480a
Vladyslav Tsyvinda <vladyslav.tsyvinda@tallium.com>
2024-10-25 14:37:04 +0300

    Minor update (cloud debugging)

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,26 +1,26 @@
 import {Router} from "express";
-import {monitoringRequestMs} from "@/monitoring";
-import redisClient from "@/redis/client";
+// import {monitoringRequestMs} from "@/monitoring";
+// import redisClient from "@/redis/client";
 import User from "@/database/models/user";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const end = monitoringRequestMs.startTimer();
+    // const end = monitoringRequestMs.startTimer();
     try {
-        let cachedUsers = await redisClient.get('users');
-
-        if (cachedUsers) {
-            res.json(JSON.parse(cachedUsers));
-        } else {
+        // let cachedUsers = await redisClient.get('users');
+        //
+        // if (cachedUsers) {
+        //     res.json(JSON.parse(cachedUsers));
+        // } else {
             const users = await User.findAll();
-            await redisClient.setEx('users', 60, JSON.stringify(users));
+            // await redisClient.setEx('users', 60, JSON.stringify(users));
             res.json(users);
-        }
+        // }
     } catch (err: any) {
         res.status(500).send(err.message);
     } finally {
-        end({ method: req.method, route: req.route.path, code: res.statusCode });
+        // end({ method: req.method, route: req.route.path, code: res.statusCode });
     }
 });
 
